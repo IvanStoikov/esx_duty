f25734d3182c769e5593a3f0884c98d2b3bd7806
Ivan Stoikov <89320750+IvanStoikov@users.noreply.github.com>
2021-08-22 11:50:11 +0300

    Add files via upload

---
+++ esx_duty/config.lua
@@ -0,0 +1,44 @@
+Config                            = {}
+Config.DrawDistance               = 100.0
+--language currently available EN and SV
+Config.Locale                     = 'en'
+
+Config.Zones = {
+
+  PoliceDuty = {
+    Pos   = { x = 443.13, y = -982.13, z = 30.69 },
+    Size = { x = 0.8, y = 0.8, z = 0.8 },
+    Color = { r = 255, g = 0, b = 0 },  
+    Type  = 29, Rotate = true,
+  },
+
+  AmbulanceDuty = {
+    Pos = { x = 309.89, y = -596.59, z = 43.2 },
+    Size = { x = 0.8, y = 0.8, z = 0.8 },
+    Color = { r = 255, g = 0, b = 0 },
+    Type = 29, Rotate = true,
+  },
+
+  StateDuty = {
+    Pos = { x = 246.05, y = -1597.63, z = 31.33 },
+    Size = { x = 0.8, y = 0.8, z = 0.8 },
+    Color = { r = 255, g = 0, b = 0 },
+    Type = 29, Rotate = true,
+  },
+
+  MechanicDuty = {
+    Pos = { x = 965.61, y = -3005.15, z = -39.64 },
+    Size = { x = 0.8, y = 0.8, z = 0.8 },
+    Color = { r = 255, g = 0, b = 0 },
+    Type = 29, Rotate = true,
+  },
+
+  
+  TaxiDuty = {
+    Pos = { x = 973.81, y = -101.53, z = 74.75 },
+    Size = { x = 0.8, y = 0.8, z = 0.8 },
+    Color = { r = 255, g = 0, b = 0 },
+    Type = 29, Rotate = true,
+  },
+
+}
